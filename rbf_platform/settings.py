04b029c6b788f965cd2469cb5d731bfbb5f914bc
ianjorin <israelwhiz@gmail.com>
2024-06-21 16:02:22 -0400

    base add

--- rbf_platform/settings.py
+++ rbf_platform/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'user_management',
+    'base',
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -76,8 +76,12 @@ WSGI_APPLICATION = "rbf_platform.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "rbf_platform",
+        "USER": "",
+        "PASSWORD": "",
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
     }
 }
 
