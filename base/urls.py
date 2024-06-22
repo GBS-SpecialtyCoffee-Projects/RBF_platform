f7a17bacb557cab8129a4561c2c96143ac683d6a
Kei <104012356+Kei-Nie@users.noreply.github.com>
2024-06-22 16:40:35 -0400

    Update urls.py

--- base/urls.py
+++ base/urls.py
@@ -8,4 +8,11 @@ urlpatterns = [
     path('roasters/', views.roasters, name='roasters'),
     path('create_account/', views.create_account, name='create_account'),
     path('farmer_dashboard/', views.farmer_dashboard, name='farmer_dashboard'),
+    path('upload/', views.upload_photo, name='upload_photo'),
+    path('upload/success/', views.upload_success, name='upload_success'),
+    path('add_roaster/', views.add_roaster, name='add_roaster'),
+    path('add_roaster/success/', views.add_roaster_success, name='add_roaster_success'),
+    path('add_roaster_photo/', views.add_roaster_photo, name='add_roaster_photo'),
+    path('add_roaster_photo/success/', views.add_roaster_photo_success, name='add_roaster_photo_success'),
 ]
+
