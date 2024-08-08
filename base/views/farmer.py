3a59ab42330d5849623568bfc772adbf490488ce
ianjorin <israelwhiz@gmail.com>
2024-08-08 11:06:26 -0400

    profile page fetch from db

--- base/views/farmer.py
+++ base/views/farmer.py
@@ -4,45 +4,47 @@ from base.models import Roaster, MeetingRequest, Farmer,FarmerPhoto
 from django.contrib import messages
 
 def farmer_dashboard(request):
-    if request.user.group != 'farmer':
-        return redirect('roaster_dashboard')
+    return(render(request, 'base/farmer_dashboard.html'))
+# def farmer_dashboard(request):
+#     if request.user.group != 'farmer':
+#         return redirect('roaster_dashboard')
 
-    roasters = Roaster.objects.all()
-    meeting_requests_as_requestee = MeetingRequest.objects.filter(requestee=request.user)
-    meeting_requests_as_requester = MeetingRequest.objects.filter(requester=request.user)
-    farmer_profile = Farmer.objects.filter(user=request.user).first()
-    farmer_photos = FarmerPhoto.objects.filter(user=request.user)
-    pending_meetings = MeetingRequest.objects.filter(
-        requester=request.user, status='accepted'
-    ) | MeetingRequest.objects.filter(
-        requestee=request.user, status='accepted'
-    )
+#     roasters = Roaster.objects.all()
+#     meeting_requests_as_requestee = MeetingRequest.objects.filter(requestee=request.user)
+#     meeting_requests_as_requester = MeetingRequest.objects.filter(requester=request.user)
+#     farmer_profile = Farmer.objects.filter(user=request.user).first()
+#     farmer_photos = FarmerPhoto.objects.filter(user=request.user)
+#     pending_meetings = MeetingRequest.objects.filter(
+#         requester=request.user, status='accepted'
+#     ) | MeetingRequest.objects.filter(
+#         requestee=request.user, status='accepted'
+#     )
 
-    # Check the number of pending or accepted meetings
-    active_meetings_count = MeetingRequest.objects.filter(
-        requester=request.user, status__in=['pending', 'accepted']
-    ).count()
+#     # Check the number of pending or accepted meetings
+#     active_meetings_count = MeetingRequest.objects.filter(
+#         requester=request.user, status__in=['pending', 'accepted']
+#     ).count()
 
-    can_request_meetings = active_meetings_count < 5
+#     can_request_meetings = active_meetings_count < 5
 
-    if request.method == 'POST':
-        form = FarmerProfileForm(request.POST, instance=farmer_profile)
-        if form.is_valid():
-            form.save()
-            return redirect('farmer_dashboard')
-    else:
-        form = FarmerProfileForm(instance=farmer_profile)
+#     if request.method == 'POST':
+#         form = FarmerProfileForm(request.POST, instance=farmer_profile)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('farmer_dashboard')
+#     else:
+#         form = FarmerProfileForm(instance=farmer_profile)
+#     return render(request, 'base/farmer_dashboard.html', {
+#         'roasters': roasters,
+#         'meeting_requests_as_requestee': meeting_requests_as_requestee,
+#         'meeting_requests_as_requester': meeting_requests_as_requester,
+#         'farmer_profile': farmer_profile,
+#         'farmer_photos': farmer_photos,
+#         'pending_meetings': pending_meetings,
+#         'can_request_meetings': can_request_meetings,
+#         'form': form,
+#     })
 
-    return render(request, 'base/farmer_dashboard.html', {
-            'roasters': roasters,
-            'meeting_requests_as_requestee': meeting_requests_as_requestee,
-            'meeting_requests_as_requester': meeting_requests_as_requester,
-            'farmer_profile': farmer_profile,
-            'farmer_photos': farmer_photos,
-            'pending_meetings': pending_meetings,
-            'can_request_meetings': can_request_meetings,
-            'form': form,
-        })
 
 def upload_photo(request):
     if request.method == 'POST':
