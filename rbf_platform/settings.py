f2c7eae1a5d5cdeeb27cc6dbd45856cd0e27f589
ianjorin <israelwhiz@gmail.com>
2026-05-22 14:03:37 -0400

    fix collectstatic on render

--- rbf_platform/settings.py
+++ rbf_platform/settings.py
@@ -210,12 +210,13 @@ if USE_S3:
     STATICFILES_DIRS = (
         os.path.join(BASE_DIR, "static/"),
     )
-    # STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles") 
+    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 else:
     STATIC_URL = "/static/"
     STATICFILES_DIRS = (
         os.path.join(BASE_DIR, "static/"),
     )
+    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
     MEDIA_URL = "/media/"
     MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
