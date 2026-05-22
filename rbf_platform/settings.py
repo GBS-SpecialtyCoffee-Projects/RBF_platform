17b362aae48c41d7486a4e4024e579514931a577
ianjorin <israelwhiz@gmail.com>
2026-05-22 15:26:43 -0400

    bugfix:trying to see images

--- rbf_platform/settings.py
+++ rbf_platform/settings.py
@@ -57,7 +57,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    # 'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -210,7 +210,7 @@ if USE_S3:
     STATICFILES_DIRS = (
         os.path.join(BASE_DIR, "static/"),
     )
-    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+    # STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 else:
     STATIC_URL = "/static/"
     STATICFILES_DIRS = (
